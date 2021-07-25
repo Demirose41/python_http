53563043e2146291d4c3029c906675ce482d633f
demirose41 <demirose41@gmail.com>
2021-07-24 19:21:04 -0700

    2. added conditional check for wether user input returns a single responce, multiple, or none. also formatted joke to return as a string

--- api_python.py
+++ api_python.py
@@ -1,6 +1,33 @@
+# import requests
+# url = "https://icanhazdadjoke.com/"
+
+# response = requests.get(url, headers={"Accept": "application/json"})
+# data = response.json()
+# print(response.text)
+# print(data['joke'])
+
 import requests
-url = "https://icanhazdadjoke.com/"
+from random import choice
+user_input = input("enter search term\n")
+url = "https://icanhazdadjoke.com/search"
+res = requests.get(
+    url, 
+    headers={"Accept": "application/json"},
+    params={"term": user_input, 'limit': 1}
+    ).json()
 
-response = requests.get(url, headers={"Accept": "text/plain"})
 
-print(response.text)
+results = res['results']
+total_jokes = res['total_jokes']
+
+if total_jokes > 1:
+    print(f"I found {total_jokes} jokes about {user_input}. Here is one:")
+    print(choice(results)["joke"])
+elif total_jokes == 1 :
+    print(f"there is A jokes about {user_input}\n")
+    print(choice(results)["joke"])
+else:
+    print(f"Sorry, couldn't find a joke with the term {user_input}")
+
+
+
